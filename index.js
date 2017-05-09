00a3705f7651df6cb546852ab8749ec22bdc9bad
Jake Overall <joverall@gmail.com>
2017-05-09 12:09:29 -0600

    adds mongoose

--- index.js
+++ index.js
@@ -1,69 +1,107 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
+
 var server = express()
 var port = 3000
+
+
+// Database Stuff
+var mongoose = require('mongoose')
+var connectionstring = 'mongodb://student:student@ds147900.mlab.com:47900/books'
+var connection = mongoose.connection;
+// DOES NOT CHANGE
+mongoose.connect(connectionstring, {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+});
+
+connection.on('error', function (err) {
+  console.log('THERE WAS A CONNECTION PROBLEM', err)
+})
+
+connection.once('open', function () {
+  console.log('We are now connected to the books database')
+  server.listen(port, function () {
+    console.log('YEP its working', 'http://localhost:' + port)
+  })
+})
+
+
+
+
+
 
 // Parses the request data into json
 // gives access to `req.body`
 server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 
 
-server.get('/', function(req, res, next){
+server.get('/', function (req, res, next) {
   res.send(200, "Hello I hear you....")
 })
 
-server.get('/banana', function(req, res, next){
+server.get('/banana', function (req, res, next) {
 
   res.send('<img src="https://media.mercola.com/assets/images/foodfacts/banana-og.jpg" height="200"/>')
 
 })
 
-var people = [{
-  name: 'Luke Skywalker'
-}, {
-  name: 'Obi-Wan Konobi'
-}]
-
-server.get('/people', function(req, res, next){
-  res.send(people)
+server.get('/people', function (req, res, next) {
+  Person.find({}).then(function (people) {
+    res.send(people)
+  })
 })
 
-server.get('/people/:id', function(req, res, next){
+server.get('/people/search', function (req, res, next) {
+  var query = req.query
+  Person.find(query).then(function (people) {
+    res.send(people)
+  })
+})
+
+server.get('/people/:id', function (req, res, next) {
   var id = req.params.id
-  console.log(id)
-  if(people[id]){
-    res.send(people[id])
-  }else{
-    res.send(404, {
-      error: {
-        message: "Sorry no person at id" + id
-      }
-    })
-  }
+  Person.findById(id).then(function (person) {
+    res.send(person)
+  }).catch(function (e) {
+    res.send(e)
+  })
 })
 
 
-server.post('/people', function(req, res, next){
+
+server.put('/people/:id', function (req, res, next) {
+  var id = req.params.id
+  var updatedPerson = req.body.person
+  Person.findByIdAndUpdate(id, updatedPerson)
+    .then(function () {
+      res.send(updatedPerson)
+    })
+
+
+})
+
+server.post('/people', function (req, res, next) {
 
   var newPerson = req.body.person
-  var secret = req.body.secret
 
-  if(newPerson.name && secret == "these are not the droids you are looking for"){
-    people.push(newPerson)
-    res.send('Okay')
-  }else{
-    res.send(401, 'sorry you must have a name to add a person')
-  }
+  Person.create(newPerson)
+    .then(function (newlyCreatedPerson) {
+      res.send(newlyCreatedPerson)
+    })
+
 })
 
-
-
-
-
-
-
-server.listen(port, function(){
-  console.log("The server is working and listening for request on port: ", port)
+// PERSON IN THE DATABASE
+var Schema = mongoose.Schema
+var PersonSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  hasHair: { type: Boolean, required: true, default: true },
+  glasses: { type: String, default: 'none' }
 })
+var Person = mongoose.model('Person', PersonSchema)
+
+
 
